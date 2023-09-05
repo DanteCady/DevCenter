2fe7f428f6efa83491fd40ec0459a9ba6e0d52da
DanteCady <37083142+DanteCady@users.noreply.github.com>
2023-09-05 11:22:10 -0400

    Updated release notes to have accordian drop downs and new layout.

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const YAML = require('js-yaml');
 const cors = require('cors');
-const markdown = require('markdown-it')();
+const bodyParser = require('body-parser'); // added for parsing JSON in POST requests
 const app = express();
+
 app.use(cors());
+app.use(bodyParser.json()); // added for parsing JSON in POST requests
 
 // Get list of all available APIs
 app.get('/api/docs/list', (req, res) => {
@@ -46,7 +48,57 @@ app.get('/api/release-notes', (req, res) => {
     }
 });
 
+// Fetch the specific version of release notes in Markdown
+app.get('/api/release-notes/version', (req, res) => {
+    const version = req.query.version || 'latest'; // If no version specified, default to 'latest'
+    const releaseNotesPath = path.join(__dirname, `./releaseNotes/releaseNotes_${version}.md`);
+    if (fs.existsSync(releaseNotesPath)) {
+        const releaseNotes = fs.readFileSync(releaseNotesPath, 'utf8');
+        res.json({ markdown: releaseNotes });
+    } else {
+        res.status(404).json({ message: 'Release notes not found for this version' });
+    }
+});
+
+// Fetch the list of versions for which release notes are available
+app.get('/api/release-notes/versions', (req, res) => {
+    const releaseNotesDir = path.join(__dirname, './releaseNotes');
+    fs.readdir(releaseNotesDir, (err, files) => {
+        if (err) {
+            return res.status(500).json({ message: 'Unable to scan directory', error: err });
+        }
+        const versions = files.map(file => file.replace('releaseNotes_', '').replace('.md', ''));
+        res.json({ versions });
+    });
+});
+
+// Collect user feedback for specific sections in the release notes
+app.post('/api/release-notes/feedback', (req, res) => {
+    const { section, vote } = req.body;
+    // Store this data in a database or some persistent store.
+    res.json({ message: 'Feedback received' });
+});
+
+// Subscription API for email notification
+app.post('/api/subscribe', (req, res) => {
+    const { email } = req.body;
+    // Store this email in a database or some persistent store.
+    res.json({ message: 'Subscribed successfully' });
+});
+
+// Downloadable assets related to release notes
+app.get('/api/release-notes/download/:assetName', (req, res) => {
+    const assetName = req.params.assetName;
+    const assetPath = path.join(__dirname, `./releaseNotes/assets/${assetName}`);
+    if (fs.existsSync(assetPath)) {
+        res.download(assetPath); // Trigger file download
+    } else {
+        res.status(404).json({ message: 'Asset not found' });
+    }
+});
 
 app.listen(3001, () => {
     console.log('Server running on http://localhost:3001');
 });
+
+
--- src/views/releaseNotes/releasenotes.js
+++ src/views/releaseNotes/releasenotes.js
@@ -2,139 +2,151 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import ReactMarkdown from 'react-markdown';
 import {
-  Container,
-  Paper,
-  Typography,
-  Box,
-  Divider,
-  CssBaseline,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Breadcrumbs,
-  Link,
-  ThemeProvider,
-  Tooltip,
-  createTheme} from '@mui/material';
+  Container, Paper, Typography, Box, Divider, CssBaseline, AppBar, Toolbar, IconButton,
+  Accordion, AccordionSummary, AccordionDetails, Select, MenuItem, Tooltip, createTheme,
+  ThemeProvider, Breadcrumbs, Link
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import NewReleasesIcon from '@mui/icons-material/NewReleases';
 import BuildIcon from '@mui/icons-material/Build';
 import BugReportIcon from '@mui/icons-material/BugReport';
-import MenuIcon from '@mui/icons-material/Menu';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
-import GetAppIcon from '@mui/icons-material/GetApp';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
 import { useDarkMode } from '../../DarkModeContext';
 import { useLocation, useNavigate } from 'react-router-dom';
-import MaintenanceNotification from '../../components/maintenance';
 
-const renderers = {
-  heading: ({ level, children }) => {
-    const text = children[0]?.props?.children;
-    if (text.includes("🆕")) {
-      return (
-        <Typography variant={`h${level}`} style={{ color: '#4caf50' }}>
-          <NewReleasesIcon /> {text.replace("🆕", "")}
-        </Typography>
-      );
-    }
-    if (text.includes("🔧")) {
-      return (
-        <Typography variant={`h${level}`} style={{ color: '#2196f3' }}>
-          <BuildIcon /> {text.replace("🔧", "")}
-        </Typography>
-      );
-    }
-    if (text.includes("🐛")) {
-      return (
-        <Typography variant={`h${level}`} style={{ color: '#f44336' }}>
-          <BugReportIcon /> {text.replace("🐛", "")}
-        </Typography>
-      );
-    }
-    return <Typography variant={`h${level}`}>{text}</Typography>;
-  },
-};
 
 const ReleaseNotes = () => {
-  const { darkMode, setDarkMode } = useDarkMode(); 
+  const { darkMode, setDarkMode } = useDarkMode();
   const [markdownContent, setMarkdownContent] = useState('');
-
+  const [selectedVersion, setSelectedVersion] = useState('1.0');
+  const [sections, setSections] = useState({});
+  const [feedback, setFeedback] = useState({});
   const location = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Fetch the Markdown file from the Express server
-    axios.get('http://localhost:3001/api/release-notes')
+    axios.get(`http://localhost:3001/api/release-notes?version=${selectedVersion}`)
       .then((res) => {
-        setMarkdownContent(res.data.markdown);
+        const markdown = res.data.markdown;
+        setMarkdownContent(markdown);
+
+        const splitByNewLine = markdown.split('\n');
+        let currentCategory = '';
+        const parsedSections = {};
+
+        splitByNewLine.forEach((line) => {
+          if (line.includes('🆕')) {
+            currentCategory = 'New Features';
+            parsedSections[currentCategory] = '';
+          } else if (line.includes('🔧')) {
+            currentCategory = 'Enhancements';
+            parsedSections[currentCategory] = '';
+          } else if (line.includes('🐛')) {
+            currentCategory = 'Bug Fixes';
+            parsedSections[currentCategory] = '';
+          }
+
+          if (currentCategory) {
+            parsedSections[currentCategory] += line + '\n';
+          }
+        });
+
+        setSections(parsedSections);
       })
       .catch((err) => {
         console.error("Failed to fetch release notes:", err);
       });
-  }, []);
+  }, [selectedVersion]);
 
  const handleThemeChange = () => {
     setDarkMode(!darkMode);
   };
 
-const theme = createTheme({
-  palette: {
-    mode: darkMode ? 'dark' : 'light',
-    ...(darkMode && {
-      background: {
-        default: '#37383a',
-      },
-    }),
-  },
-});
+  const theme = createTheme({
+    palette: {
+      mode: darkMode ? 'dark' : 'light',
+      ...(darkMode && {
+        background: {
+          default: '#37383a',
+        },
+      }),
+    },
+  });
 
-const pathnameSegments = location.pathname.split('/').filter(segment => segment !== '');
+  const pathnameSegments = location.pathname.split('/').filter(segment => segment !== '');
 
   return (
-        <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-         <div className={darkMode ? 'dark-mode' : ''}>
-       <AppBar position="static" sx={{ backgroundColor: darkMode ? '#37383a' : '#0c51a1' }}>
-  <Toolbar>
-    {location.pathname !== '/' && (
-      <IconButton edge="start" color="inherit" onClick={() => navigate(-1)}>
-        <ArrowBackIcon />
-      </IconButton>
-    )}
-    <Breadcrumbs sx={{ flexGrow: 1 }}>
-      {pathnameSegments.map((segment, index) => (
-        <Link key={index} href="/" sx={{ color: 'white' }}>
-          {segment}
-        </Link>
-      ))}
-    </Breadcrumbs>
-    <Tooltip title={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}>
-      <IconButton onClick={handleThemeChange} color="inherit">
-        {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
-      </IconButton>
-    </Tooltip>
-  </Toolbar>
-</AppBar>
+      <AppBar position="static" sx={{ backgroundColor: darkMode ? '#37383a' : '#0c51a1' }}>
+        <Toolbar>
+          {location.pathname !== '/' && (
+            <IconButton edge="start" color="inherit" onClick={() => navigate(-1)}>
+              <ArrowBackIcon />
+            </IconButton>
+          )}
+          <Breadcrumbs sx={{ flexGrow: 1 }}>
+            {pathnameSegments.map((segment, index) => (
+              <Link key={index} href="/" sx={{ color: 'white' }}>
+                {segment}
+              </Link>
+            ))}
+          </Breadcrumbs>
+          <Tooltip title={darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}>
+            <IconButton onClick={handleThemeChange} color="inherit">
+              {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+          </Tooltip>
+          <Select
+            value={selectedVersion}
+            onChange={(e) => setSelectedVersion(e.target.value)}
+          >
+            <MenuItem value="1.0">Version 1.0</MenuItem>
+            <MenuItem value="2.0">Version 2.0</MenuItem>
+          </Select>
+        </Toolbar>
+      </AppBar>
+    <Container>
+      <Box p={3} mt={3}>
+        <Paper elevation={3}>
+          <Box p={3}>
+            <Typography variant="h4" gutterBottom>
+              Latest Updates
+            </Typography>
+            <Divider />
+            <Select
+              value={selectedVersion}
+              onChange={(e) => setSelectedVersion(e.target.value)}
+            >
+              <MenuItem value="1.0">Version 1.0</MenuItem>
+              <MenuItem value="2.0">Version 2.0</MenuItem>
+            </Select>
 
-      <Container>
-        <Box p={3} mt={3}>
-          <Paper elevation={3}>
-            <Box p={3}>
-              <Typography variant="h4" gutterBottom>
-                Latest Updates
-              </Typography>
-              <Divider />
-              {/* Render the markdown content */}
-              <ReactMarkdown components={renderers}>
-                {markdownContent}
-              </ReactMarkdown>
-            </Box>
-          </Paper>
-        </Box>
+            {Object.keys(sections).map((key, index) => (
+              <Accordion key={index}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls={`panel${index}-content`}
+                  id={`panel${index}-header`}
+                >
+                  <Typography variant="h6">{key}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <ReactMarkdown>
+                    {sections[key]}
+                  </ReactMarkdown>
+                </AccordionDetails>
+              </Accordion>
+            ))}
+          </Box>
+        </Paper>
+      </Box>
       </Container>
-     </div>
-    </ThemeProvider>
+      </ThemeProvider>
   );
 };
 
